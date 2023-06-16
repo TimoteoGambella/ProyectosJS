16736bc17bbe7e9b3f5a817f09cfe79edaa8f8f4
TimoteoGambella <timi.gambella@hotmail.com>
2023-06-16 13:06:12 -0300

    p20

--- src/views/Project20.jsx
+++ src/views/Project20.jsx
@@ -5,93 +5,33 @@ import mic from "../asstes/project20/mic.png"
 export default function Project20(){  
 
   const [numb,setNumb]=useState(Math.floor(Math.random() * 100) + 1)
-  const [recog,setRecog]=useState("")
+
+  window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const [recog,setRecog]=useState(new window.SpeechRecognition())
+  const [speakAudio,setSpeakAudio]=useState("")
+
+  const [newSpeak,setNewSpeak]=useState(false)
 
   useEffect(() => {
-    if(recog===""){
-      setRecog(new window.SpeechRecognition())
-    }else{
-      recognition.addEventListener('result', ()=>console.log("HOLA"));
-      recognition.addEventListener('end', () => console.log("HOLA"));
+    recog.start()
 
-    }
-    console.log(recog)
+    recog.addEventListener('result', (e)=>onSpeak(e))
+    recog.addEventListener('end',()=>recog.start())
   }, [recog]);
 
-  const msgEl = document.getElementById('msg');
-
-  const randomNum = getRandomNumber();
-    
-  window.SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  
-  let recognition = new window.SpeechRecognition();
-  
-  // Start recognition and game
-  recognition.start();
-  
+  useEffect(() => {
+    setNewSpeak(false)
+  }, [newSpeak]);
+        
   // Capture user speak
   function onSpeak(e) {
     const msg = e.results[0][0].transcript;
-  
-    writeMessage(msg);
-    checkNumber(msg);
+    setNewSpeak(true)
+    setSpeakAudio(msg)
   }
-  
-  // Write what user speaks
-  function writeMessage(msg) {
-    msgEl.innerHTML = `
-      <div>You said: </div>
-      <span class="box">${msg}</span>
-    `;
-  }
-  
-  // Check msg against number
-  function checkNumber(msg) {
-    const num = +msg;
-  
-    // Check if valid number
-    if (Number.isNaN(num)) {
-      msgEl.innerHTML += '<div>That is not a valid number</div>';
-      return;
-    }
-  
-    // Check in range
-    if (num > 100 || num < 1) {
-      msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
-      return;
-    }
-  
-    // Check number
-    if (num === randomNum) {
-      document.body.innerHTML = `
-        <h2>Congrats! You have guessed the number! <br><br>
-        It was ${num}</h2>
-        <button class="play-again" id="play-again">Play Again</button>
-      `;
-    } else if (num > randomNum) {
-      msgEl.innerHTML += '<div>GO LOWER</div>';
-    } else {
-      msgEl.innerHTML += '<div>GO HIGHER</div>';
-    }
-  }
-  
-  // Generate random number
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
-  }
-  
-  // Speak result
-  recognition.addEventListener('result', ()=>console.log("HOLA"));
-  
-  // End SR service
-  recognition.addEventListener('end', () => console.log("HOLA"));
-  
-  document.body.addEventListener('click', e => {
-    if (e.target.id === 'play-again') {
-      window.location.reload();
-    }
-  });
+
   return(
     <div className="project20">
       <img src={mic} alt="Speak" />
@@ -100,6 +40,29 @@ export default function Project20(){
 
       <h3>Speak the number into your microphone</h3>
 
-      <div id="msg" className="msg"></div>
+      <h2>Your Speek: {speakAudio}</h2>
+      {!newSpeak && 
+        <div id="msg" className="msg">
+          {
+            speakAudio==="" ? <div>Say a number</div> :
+            !Number(speakAudio) ?
+              <div>That is not a valid number</div>
+            :
+              (speakAudio > 100 || speakAudio < 1) ?
+                <div>Number must be between 1 and 100</div>
+              : Number(speakAudio) === numb ?
+                <>
+                  <h2>Congrats! You have guessed the number! It was {speakAudio}</h2>
+                  <button className="play-again" id="play-again" onClick={()=>{
+                    setNumb(Math.floor(Math.random() * 100) + 1)
+                    setSpeakAudio("")
+                  }}>Play Again</button>
+                </>
+                : speakAudio > numb ?
+                  <div>GO LOWER</div> :
+                  <div>GO HIGHER</div>
+          }
+        </div>
+      }
     </div>
   )}
